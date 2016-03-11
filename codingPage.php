056ad56e52962d2240a7c7e5ea5cb1918d183a5c
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-10 20:25:31 -0600

    switched jquery version

--- codingPage.php
+++ codingPage.php
@@ -76,9 +76,9 @@
 
 <script src="ace-builds-master/src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
 
-<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> 
+<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> -->
 <script> 
 	 	$('#myframe').contents().find('body').append('asd').end()
 	        .find('body').append('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"><\/script>').end()
